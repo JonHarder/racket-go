ec0aabc58ae00ec3461828f549dfb7710a40eb9d
Jon Harder <jonharder6@gmail.com>
2015-02-04 15:48:23 -0600

    added initial computer player. it should be noted it will always try to play at (0 . 0)

---
+++ computer.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+(define (decide-move board)
+  "given the current state of the board, determine where to place its
+   next move. For now computer is assumed to be the white stones"
+  '(0 . 0))
