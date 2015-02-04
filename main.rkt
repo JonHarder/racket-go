6d430209f11694449f6a816aa73175d83c5a7f5a
Jon Harder <jonharder6@gmail.com>
2015-02-04 13:26:22 -0600

    Added command line load flag and propper loading of saves

--- main.rkt
+++ main.rkt
@@ -22,13 +22,14 @@
 
 (define (save-game board player [location #f])
   (if location
-      (display-to-file board location #:exists 'replace)
+      (display-to-file (list player board) location #:exists 'replace)
       (let* ([home (find-system-path 'home-dir)]
              [path (string-append (path->string home) "go.save")])
-        (display-to-file board path #:exists 'replace))))
+        (display-to-file (list player board) path #:exists 'replace))))
 
 
 (define (load-game [path #f])
+  "takes (optional) file path and returns (list player board)"
   (if (not path)
       (let* ([homepath (find-system-path 'home-dir)]
              [filepath (string-append (path->string homepath) "go.save")])
@@ -58,6 +59,21 @@
               (play board (next-turn player)))])))
 
 (define (start-game)
-  (play initial-board 'black))
+  (if (load-game-file?)
+      (let ([contents (load-game (load-game-file?))])
+        (printf "~a\n" (car contents))
+        (play (cadr contents) (car contents)))
+      (play initial-board 'black)))
 
+(define load-game-file? (make-parameter #f))
+
+(define game-to-load
+  (command-line
+  #:once-each
+   [("-l" "--load")
+    file
+    "Load save file to continue previous game"
+    (load-game-file? file)]))
+
+;;;;;;;;;;;;; run game ;;;;;;;;;;;;;;;;;;
 (start-game)
